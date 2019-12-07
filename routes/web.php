9126b9ac80a57a3976ed4d8a946a45398cfa3d06
HattaMaulana <mahatta.maulana@gmail.com>
2019-12-08 02:12:09 +0700

    update adminlte template

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,13 @@ Route::group(['namespace' => 'User'], function () {
     Route::get('/kontak', 'ContactController@index')
         ->name('user.contact');
 });
+
+/*
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['namespace' => 'Admin'], function () {
+    Route::get('/admin', 'RootController@index');
+});
